af0d97029fed09824b3a45d1d8c245733f1c22ff
Hitoshi Kusano <iazekr@gmail.com>
2022-01-18 14:04:56 +0900

    add cors bridge

--- finder.js
+++ finder.js
@@ -20,8 +20,8 @@ AFRAME.registerComponent('finder', {
         var stevec = 0;
         console.log('longitude' + longitude);
         console.log('latitude' + latitude);
-        console.log(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=restaurant&rankby=distance&radius=2000&key=${API_KEY}`);
-        fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=restaurant&rankby=distance&key=${API_KEY}`)
+        console.log(`https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=restaurant&rankby=distance&radius=2000&key=${API_KEY}`);
+        fetch(`https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=restaurant&rankby=distance&key=${API_KEY}`)
             .then(function (response) {
                 if (response.status !== 200) {
                     console.log('Looks like there was a problem. Status Code: ' +
